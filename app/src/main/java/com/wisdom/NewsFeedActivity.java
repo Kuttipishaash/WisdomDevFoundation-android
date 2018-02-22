3463de879d409467fd1fe68ec7ec2585a3785fea
Prejith Premkumar <prajithpremg@gmail.com>
2018-02-22 18:50:03 +0530

    Create NewsFeed activity and adapter

--- app/src/main/java/com/wisdom/NewsFeedActivity.java
+++ app/src/main/java/com/wisdom/NewsFeedActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.w3c.dom.Document;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -22,30 +27,69 @@ import javax.net.ssl.X509TrustManager;
  */
 
 public class NewsFeedActivity extends AppCompatActivity {
+
+    private List<FeedItem> feedItems;
+    private ListView mFeedList;
+    private NewsFeedAdapter mFeedAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsfeed);
+
+        disableSecurity();
+        mFeedList = (ListView) findViewById(R.id.list_feed);
+        feedItems = new ArrayList<FeedItem>();
+
+        new JSoupLoaderTask().execute();
     }
 
     private class JSoupLoaderTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            Toast.makeText(NewsFeedActivity.this, "Load start", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+
+            mFeedAdapter = new NewsFeedAdapter(NewsFeedActivity.this, R.layout.item_newsfeed, feedItems);
+            Toast.makeText(NewsFeedActivity.this, "Load end", Toast.LENGTH_SHORT).show();
+            mFeedList.setAdapter(mFeedAdapter);
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
 
-            org.jsoup.nodes.Document doc = null;
             try {
-                doc = Jsoup.connect("https://blog.udacity.com").get();
-                Elements articleUrls = doc.select("article > header > h1 > a");
+                List<String> headings = new ArrayList<String>();
+                List<String> actualUrls = new ArrayList<String>();
+                List<String> imageUrls = new ArrayList<String>();
+
+                FeedItem feedItem = new FeedItem();
+                Document doc = Jsoup.connect("https://blog.udacity.com").get();
+                Elements articles = doc.select("article > header > h1 > a");
+
+                for (Element article : articles) {
+                    headings.add(article.text());
+                    actualUrls.add(article.attr("href"));
+                }
+
+                Elements images = doc.select("article > div > p > img");
+                for (Element image : images) {
+                    imageUrls.add(image.attr("src"));
+                }
+
+                for(int i = 0; i < 7; i++) {
+                    feedItem = new FeedItem();
+                    feedItem.setArticleHeading(headings.get(i));
+                    feedItem.setActualUrl(actualUrls.get(i));
+                    feedItem.setImageUrl(imageUrls.get(i));
+                    feedItems.add(feedItem);
+                }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -58,30 +102,26 @@ public class NewsFeedActivity extends AppCompatActivity {
         TrustManager[] trustAllCerts = new TrustManager[]{
                 new X509TrustManager() {
 
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers()
-                    {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                         return null;
                     }
-                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
-                    {
+
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
                         //No need to implement.
                     }
-                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
-                    {
+
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
                         //No need to implement.
                     }
                 }
         };
 
         // Install the all-trusting trust manager
-        try
-        {
+        try {
             SSLContext sc = SSLContext.getInstance("SSL");
             sc.init(null, trustAllCerts, new java.security.SecureRandom());
             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("ERR_DISABLE_SECURITY", "Security has note been dsiabled");
         }
 
