197f4f35744a6ef2e46f190f23e2786a678ddc69
Prabha Shankar <shankar75031@gmail.com>
2018-02-22 22:00:40 +0530

    intro screens in progress

--- app/src/main/java/com/wisdom/MainActivity.java
+++ app/src/main/java/com/wisdom/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wisdom;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -20,13 +21,13 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 1;
-
-
+    private static final String PREF_FILE = "UserPreferences";
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
-
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mFirebaseAuthStateListner;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
 
     private ProgressBar mProgressBar;
@@ -40,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference("/");
 
+        mSharedPreferences = getApplicationContext().getSharedPreferences(PREF_FILE, 0);
+        mEditor = mSharedPreferences.edit();
+        boolean previouslyStarted = mSharedPreferences.getBoolean("first_start", false);
+
+        if (!previouslyStarted) {
+            mEditor.putBoolean("first_start", true);
+            mEditor.apply();
+            startActivity(new Intent(MainActivity.this, IntroActivity.class));
+        }
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseAuthStateListner = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -51,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
                     UserInfo user = new UserInfo(2, userEmail);
                     mDatabaseReference.child("user_info/" + userID).setValue(user);
                     Toast.makeText(MainActivity.this, "Logged in as : " + userEmail, Toast.LENGTH_SHORT).show();
-
                 } else {
                     startActivityForResult(
                             AuthUI.getInstance()
@@ -85,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
         if (requestCode == RC_SIGN_IN) {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
@@ -94,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
 
                 startActivity(new Intent(MainActivity.this, NewsFeedActivity.class));
                 Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
-
                 finish();
             } else {
                 // Sign in failed
