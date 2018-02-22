f71cb7738678d63ea3b975f309b9d7edc1e092b3
Prabha Shankar <shankar75031@gmail.com>
2018-02-22 16:29:11 +0530

    email sign in done google sign in needs sha key to be added in firebase inorder to work

--- app/src/main/java/com/wisdom/MainActivity.java
+++ app/src/main/java/com/wisdom/MainActivity.java
@@ -1,27 +1,27 @@
 package com.wisdom;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import org.w3c.dom.Text;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 1;
+
+    private GoogleSignInClient mGoogleSignInClient;
 
 
     private FirebaseDatabase mFirebaseDatabase;
@@ -30,7 +30,12 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mFirebaseAuthStateListner;
 
-    private TextView mSampleTextView;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginButton;
+    private ProgressBar mProgressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,15 +45,10 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuthStateListner = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser firebaseUser;
-                firebaseUser = firebaseAuth.getCurrentUser();
-                if(firebaseUser != null){
-                    //signed in
-                    mSampleTextView.setText("Logged in as " + firebaseUser.getDisplayName());
-                }
-                else{
-                    //signed out
-                    mSampleTextView.setText("not signed in");
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Toast.makeText(MainActivity.this, "Logged in as : " + firebaseUser.getDisplayName(), Toast.LENGTH_SHORT);
+                } else {
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
@@ -61,6 +61,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        mFirebaseAuth.addAuthStateListener(mFirebaseAuthStateListner);
+
+        /*
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mLoginButton.setVisibility(View.GONE);
+                //mProgressBar.setVisibility(View.VISIBLE);
+                mFirebaseAuth.signInWithEmailAndPassword(mEmailField.getText().toString(), mPasswordField.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(MainActivity.this, NewsFeedActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                                }
+                             //   mLoginButton.setVisibility(View.VISIBLE);
+                               // mProgressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
+*/
+
+
     }
 
     @Override
@@ -75,8 +103,10 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuth.addAuthStateListener(mFirebaseAuthStateListner);
     }
 
-    private void initViews(){
-        mSampleTextView = (TextView) findViewById(R.id.text_sample);
+    private void initViews() {
+        //mEmailField = (EditText) findViewById(R.id.et_email);
+        //mPasswordField = (EditText) findViewById(R.id.et_password);
+        //mLoginButton = (Button) findViewById(R.id.btn_login);
     }
 
 }
