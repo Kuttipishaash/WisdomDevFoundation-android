926b48af5a5c8511443d09b9b92e7ed952f50d2a
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-16 14:24:23 +0300

    add tint to image in news feed viewing activity

--- app/src/main/java/com/wisdom/NewsViewActivity.java
+++ app/src/main/java/com/wisdom/NewsViewActivity.java
@@ -72,6 +72,7 @@ public class NewsViewActivity extends AppCompatActivity {
                         .into(new SimpleTarget<Drawable>() {
                             @Override
                             public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                                resource.setAlpha(150);
                                 mToolbarLayout.setBackground(resource);
                             }
                         });
