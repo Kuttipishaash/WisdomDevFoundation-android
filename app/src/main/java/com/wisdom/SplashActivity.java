171ca58bc69b7fed5c2b35a7397542ea34aeec55
mebin <mebinjohnson@gmail.com>
2018-03-17 17:21:00 +0530

    Navigation Drawer Grouped and About Us Updated

--- app/src/main/java/com/wisdom/SplashActivity.java
+++ app/src/main/java/com/wisdom/SplashActivity.java
@@ -33,7 +33,7 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
