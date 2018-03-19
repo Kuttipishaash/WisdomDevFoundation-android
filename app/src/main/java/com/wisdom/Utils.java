e6a5687643484abe19395be17b3243ce95cc52d6
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-19 20:40:19 +0300

    created utils class for better code management

---
+++ app/src/main/java/com/wisdom/Utils.java
@@ -0,0 +1,23 @@
+package com.wisdom;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by praji on 19-Mar-18.
+ */
+
+public class Utils {
+    public static FeedClient getFeedClientRef() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://wisdominitiatives.org")
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder
+                .client(httpClient.build())
+                .build();
+
+        return retrofit.create(FeedClient.class);
+    }
+}
