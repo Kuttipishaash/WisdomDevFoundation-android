c427672615f9edddc874e16a94126e615b299b09
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-20 11:31:24 +0300

    document utils.java

--- app/src/main/java/com/wisdom/Utils.java
+++ app/src/main/java/com/wisdom/Utils.java
@@ -11,15 +11,23 @@ import retrofit2.converter.gson.GsonConverterFactory;
 //TODO: Move network check here
 
 public class Utils {
+
     public static FeedClient getFeedClientRef() {
+
+        //Create OkHttpClient
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        //Initialize Retrofit API Builder
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("http://wisdominitiatives.org")
                 .addConverterFactory(GsonConverterFactory.create());
+
+        //Build retrofit object with OkHttpClient builder
         Retrofit retrofit = builder
                 .client(httpClient.build())
                 .build();
 
+        //Return a retrofit instance of FeedClient Interface
         return retrofit.create(FeedClient.class);
     }
 }
